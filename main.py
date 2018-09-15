7a27eec893e1b773793b3365e70f2c120b4ce27e
geniuspaul <31869976+geniuspaul@users.noreply.github.com>
2018-09-15 01:27:24 -0500

    spotify update 2

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-import praw
-import spotify
+# import praw
+# import spotify
+#
+#
+# alexabot = praw.Reddit(user_agent = 'alexa_play_bot_2',
+#                   client_id = 'obgVOv3lTA0JbQ',
+#                   client_secret = 'sKCI6fZm9pHLy6WVUBNNuOIwV_8',
+#                   username = 'alexa_play_bot',
+#                   password = '990610Alexa')
+#
+# subreddit = alexabot.subreddit('testingground4bots')
+#
+# comments = subreddit.stream.comments()
+#
+# for comment in comments:
+#     text = comment.body
+#     author = comment.author
+#     if ('alexa play' in text.lower()) & (len(text) > 11):
+#         print (text)
+#         print (text.index('alexa play') + len('alexa play '))
+#         songtitle = text[text.index('alexa play ') + len('alexa play '):-1].split('.')[0]
+#         # songtitle = text[0:3]
+#         message = spotify.search_track(songtitle).format(author)
+#         comment.reply(message)
 
+import praw
+from youtube2 import youtube_search
 
 alexabot = praw.Reddit(user_agent = 'alexa_play_bot_2',
                   client_id = 'obgVOv3lTA0JbQ',
@@ -15,10 +39,11 @@ comments = subreddit.stream.comments()
 for comment in comments:
     text = comment.body
     author = comment.author
-    if ('alexa play' in text.lower()) & (len(text) > 11):
-        print (text)
-        print (text.index('alexa play') + len('alexa play '))
-        songtitle = text[text.index('alexa play ') + len('alexa play '):-1].split('.')[0]
-        # songtitle = text[0:3]
-        message = spotify.search_track(songtitle).format(author)
+    text = text.lower()
+
+    if 'owlbot play ' in text:
+        search = text[text.index('owlbot play ') + len('owlbot play '):] #text.replace('alexa play', '')
+        test = youtube_search(search)
+        s = "https://www.youtube.com/watch?v=" + test
+        message = ("Now Playing: " + s).format(author)
         comment.reply(message)
---
+++ youtube2.py
@@ -0,0 +1,20 @@
+from googleapiclient.discovery import build
+# from apiclient.errors import HttpError
+# from oauth2client.tools import argparser
+DEVELOPER_KEY = "AIzaSyC8Lr1783Le0ZFz_rJ_wFTLzJlTdQXpxYM"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+def youtube_search(q, max_results=50, order="relevance", token=None, location=None, location_radius=None):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+    search_response = youtube.search().list(
+        q=q,
+        type="video",
+        pageToken=token,
+        order=order,
+        part="id,snippet",
+        maxResults=max_results,
+        location=location,
+        locationRadius=location_radius
+     ).execute()
+    return (search_response['items'][0]['id']['videoId'])
