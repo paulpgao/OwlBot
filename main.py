2e9f13e41f4e3690e89cb994da413924a41e4772
geniuspaul <31869976+geniuspaul@users.noreply.github.com>
2018-09-14 22:30:53 -0500

    1

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import praw
 
-## Enter the following information about your bot
-USER_AGENT = 'alexa_play_bot_2'
-CLIENT_ID = 'obgVOv3lTA0JbQ'
-CLIENT_SECRET = 'sKCI6fZm9pHLy6WVUBNNuOIwV_8'
-USERNAME = 'alexa_play_bot'
-PASSWORD = '990610Alexa'
 
+alexabot = praw.Reddit(user_agent = 'alexa_play_bot_2',
+                  client_id = 'obgVOv3lTA0JbQ',
+                  client_secret = 'sKCI6fZm9pHLy6WVUBNNuOIwV_8',
+                  username = 'alexa_play_bot',
+                  password = '990610Alexa')
 
-## Creating a new bot instance
-bot = praw.Reddit(user_agent = USER_AGENT,
-                  client_id = CLIENT_ID,
-                  client_secret = CLIENT_SECRET,
-                  username = USERNAME,
-                  password = PASSWORD)
+subreddit = alexabot.subreddit('testingground4bots')
 
-## This subreddit is useful
-subreddit = bot.subreddit('testingground4bots')
-
-## This maintains a constant stream of comments in real time
 comments = subreddit.stream.comments()
+# posts = subreddit.stream.submissions()
 
-## Looks at every
 for comment in comments:
-    text = comment.body ## Get comment's body
-    author = comment.author ## Get comment's body
-    if 'hackrice' in text.lower():
-        ## Generate message
-        message = "Hey u/{0}, it looks like you're interested in HackRice! For more information, please visit [our website](http://hack.rice.edu)".format(author)
-        comment.reply(message) ## Send message
+    text = comment.body
+    author = comment.author
+    if 'alexa play' in text.lower():
+        message = "Now Playing: ".format(author)
+        comment.reply(message)
+
+# for post in posts:
+#     text = post.title
+#     author = post.author
+#     if 'alexa play' in post.title.lower():
+#         message = "sorry dude despacito sucks".format(author)
+#         post.reply(message)
