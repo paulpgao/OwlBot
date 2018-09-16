6831415bb2e406274df921a86a7423acc0b6e1df
Alan <abj3@rice.edu>
2018-09-15 19:48:39 -0500

    Alrighty

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
-# import praw
-# import spotify
-#
-#
-# alexabot = praw.Reddit(user_agent = 'alexa_play_bot_2',
-#                   client_id = 'obgVOv3lTA0JbQ',
-#                   client_secret = 'sKCI6fZm9pHLy6WVUBNNuOIwV_8',
-#                   username = 'alexa_play_bot',
-#                   password = '990610Alexa')
-#
-# subreddit = alexabot.subreddit('testingground4bots')
-#
-# comments = subreddit.stream.comments()
-#
-# for comment in comments:
-#     text = comment.body
-#     author = comment.author
-#     if ('alexa play' in text.lower()) & (len(text) > 11):
-#         print (text)
-#         print (text.index('alexa play') + len('alexa play '))
-#         songtitle = text[text.index('al exa play ') + len('alexa play '):-1].split('.')[0]
-#         # songtitle = text[0:3]
-#         message = spotify.search_track(songtitle).format(author)
-#         comment.reply(message)
-
 import praw
 from youtube2 import youtube_search
 from spotify2 import search_track
+import posixpath
+import urllib.parse
+import requests
+from imgurpython import ImgurClient
 
 alexabot = praw.Reddit(user_agent = 'alexa_play_bot_2',
                   client_id = 'obgVOv3lTA0JbQ',
                   client_secret = 'sKCI6fZm9pHLy6WVUBNNuOIwV_8',
                   username = 'alexa_play_bot',
                   password = '990610Alexa')
+
+client = ImgurClient(client_id = 'dd33db2d7391c65',
+                     client_secret = '7e473c05de42a24de086f965fc126fd8ce7c9c6f')
+
 
 subreddit = alexabot.subreddit('testingground4bots')
 
@@ -42,7 +25,7 @@ for comment in comments:
     f = open('comments.txt', 'r+')
     commentIDs = f.read()
     f.close()
-    if len(commentIDs) > 100:
+    if len(commentIDs) > 1000:
         open('comments.txt', 'w')
     text = comment.body
     author = comment.author
@@ -57,3 +40,6 @@ for comment in comments:
             f1 = open('comments.txt', 'a+')
             f1.write(comment.id + " ")
             f1.close()
+        if 'owlbot check' in text and 'imgur.com/' in text:
+            search = text[text.index('imgur.com') + len('imgur.com'):]
+            image = client.get_image(search)
